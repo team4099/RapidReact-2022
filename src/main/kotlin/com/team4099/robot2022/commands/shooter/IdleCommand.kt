21442ab2b8778c61fd55ecdb15b5d7b3023927cd
flash76 <30060599+flash76@users.noreply.github.com>
2022-01-29 12:42:28 -0500

    create idle command

--- src/main/kotlin/com/team4099/robot2022/commands/shooter/IdleCommand.kt
+++ src/main/kotlin/com/team4099/robot2022/commands/shooter/IdleCommand.kt
@@ -1,4 +1,17 @@
 package com.team4099.robot2022.commands.shooter
 
-class IdleCommand {
+import com.team4099.robot2022.config.Constants.ShooterConstants
+import com.team4099.robot2022.subsystems.Shooter
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class IdleCommand : CommandBase() {
+  init {
+    addRequirements(Shooter)
+  }
+
+  override fun initialize() {
+    Shooter.shooterState = ShooterConstants.ShooterState.IDLE
+  }
+
+  override fun isFinished(): Boolean = false
 }
