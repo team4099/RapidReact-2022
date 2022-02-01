6cb849830a40aa32b5b77385a9c2222799c14924
flash76 <30060599+flash76@users.noreply.github.com>
2022-01-31 19:09:49 -0500

    add openloopdrive to adjust distance but idk if it work

--- src/main/kotlin/com/team4099/robot2022/commands/shooter/AimAssist.kt
+++ src/main/kotlin/com/team4099/robot2022/commands/shooter/AimAssist.kt
@@ -1,9 +1,12 @@
 package com.team4099.robot2022.commands.shooter
 
 import com.team4099.lib.units.base.inMeters
+import com.team4099.lib.units.base.meters
 import com.team4099.lib.units.derived.rotations
 import com.team4099.lib.units.inRadiansPerSecond
 import com.team4099.lib.units.perSecond
+import com.team4099.robot2022.commands.drivetrain.OpenLoopDriveCommand
+import com.team4099.robot2022.config.Constants.VisionConstants
 import com.team4099.robot2022.config.Constants.ShooterConstants
 import com.team4099.robot2022.subsystems.Drivetrain
 import com.team4099.robot2022.subsystems.Shooter
@@ -12,6 +15,8 @@ import edu.wpi.first.wpilibj2.command.CommandBase
 import org.photonvision.targeting.PhotonTrackedTarget
 
 class AimAssist : CommandBase() {
+  private var rangeToTarget = 0.0.meters
+  private var diff = 0.0.meters
 
   init {
     addRequirements(Shooter)
@@ -20,16 +25,15 @@ class AimAssist : CommandBase() {
   }
 
   override fun execute() {
+    rangeToTarget = Vision.getRangeToBestTarget()
+    diff = VisionConstants.TARGET_RANGE - rangeToTarget
     val target = Vision.bestTarget
-    val rangeToTarget = Vision.getRangeToBestTarget()
     val offsetToTarget = Vision.getOffsetToBestTarget()
-
-    // projectile motion 🤪
-    val vt =
-      Shooter.shooterVelocity.inRadiansPerSecond * ShooterConstants.FLYWHEEL_RADIUS.inMeters
+    OpenLoopDriveCommand({ rangeToTarget.inMeters / 2 }, { 0.0 }, { 0.0 })
   }
 
   override fun isFinished(): Boolean {
-    return Vision.bestTarget == null // TODO: Update with feeder indexing status
+    val diff = VisionConstants.TARGET_RANGE - rangeToTarget
+    return (diff <= VisionConstants.RANGE_THRESHOLD && diff > 0.0.meters) || Vision.bestTarget == null
   }
 }
