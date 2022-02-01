2f98d49a41811bd5227f5e145459cc3ff00d89df
jocel-4 <jocelyngu112@gmail.com>
2022-01-31 19:06:29 -0500

    create shoot command

--- src/main/kotlin/com/team4099/robot2022/commands/shooter/ShootCommand.kt
+++ src/main/kotlin/com/team4099/robot2022/commands/shooter/ShootCommand.kt
@@ -1,4 +1,19 @@
 package com.team4099.robot2022.commands.shooter
 
-class ShootCommand {
+import com.team4099.robot2022.subsystems.Shooter
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class ShootCommand : CommandBase() {
+  init {
+    addRequirements(Shooter)
+    // addRequirements(Feeder)
+  }
+
+  override fun execute() {
+
+  }
+
+  override fun isFinished(): Boolean {
+
+  }
 }
