15b36bb67fdeeca3e1299b89b4a88ff22a0fbdc2
flash76 <30060599+flash76@users.noreply.github.com>
2022-02-18 17:39:20 -0500

    rearrange code

--- src/main/kotlin/com/team4099/robot2022/Robot.kt
+++ src/main/kotlin/com/team4099/robot2022/Robot.kt
@@ -7,6 +7,7 @@ import com.team4099.robot2022.auto.AutonomousSelector
 import com.team4099.robot2022.commands.drivetrain.OpenLoopDriveCommand
 import com.team4099.robot2022.commands.drivetrain.ResetGyroCommand
 import com.team4099.robot2022.commands.feeder.FeederCommand
+import com.team4099.robot2022.commands.feeder.FeederIdleCommand
 import com.team4099.robot2022.commands.feeder.FeederSerialize
 import com.team4099.robot2022.commands.intake.IntakeBallsCommand
 import com.team4099.robot2022.commands.intake.IntakeIdleCommand
@@ -26,7 +27,6 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.RobotController
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj2.command.CommandScheduler
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import kotlin.math.pow
 
 object Robot : TimedRobot() {
@@ -35,47 +35,42 @@ object Robot : TimedRobot() {
 
   init {
     val robotId =
-        Constants.Tuning
-            .ROBOT_ID_PINS
-            .withIndex()
-            .map { (i, pin) -> if (DigitalInput(pin).get()) 0 else 2.0.pow(i).toInt() }
-            .sum()
+      Constants.Tuning
+        .ROBOT_ID_PINS
+        .withIndex()
+        .map { (i, pin) -> if (DigitalInput(pin).get()) 0 else 2.0.pow(i).toInt() }
+        .sum()
     robotName =
-        Constants.Tuning.ROBOT_ID_MAP.getOrDefault(robotId, Constants.Tuning.RobotName.COMPETITION)
+      Constants.Tuning.ROBOT_ID_MAP.getOrDefault(robotId, Constants.Tuning.RobotName.COMPETITION)
     Logger.addEvent("Robot", "Robot Construction (running on $robotName)")
     Logger.addSource("Robot", "Battery Voltage", RobotController::getBatteryVoltage)
 
     Logger.startLogging()
 
-    Feeder.defaultCommand = FeederSerialize()
-
-    ControlBoard.runFeederIn.whileActiveOnce(FeederCommand(FeederConstants.FeederState.FORWARD_ALL))
-    ControlBoard.runFeederOut
-        .whileActiveOnce(FeederCommand(FeederConstants.FeederState.BACKWARD_ALL))
-
     Drivetrain.defaultCommand =
-        OpenLoopDriveCommand(
-            { ControlBoard.strafe.smoothDeadband(Constants.Joysticks.THROTTLE_DEADBAND) },
-            { ControlBoard.forward.smoothDeadband(Constants.Joysticks.THROTTLE_DEADBAND) },
-            { ControlBoard.turn.smoothDeadband(Constants.Joysticks.TURN_DEADBAND) })
+      OpenLoopDriveCommand(
+        { ControlBoard.strafe.smoothDeadband(Constants.Joysticks.THROTTLE_DEADBAND) },
+        { ControlBoard.forward.smoothDeadband(Constants.Joysticks.THROTTLE_DEADBAND) },
+        { ControlBoard.turn.smoothDeadband(Constants.Joysticks.TURN_DEADBAND) })
 
     ControlBoard.resetGyro.whileActiveOnce(ResetGyroCommand())
 
-    // Shooter controls
     Shooter.defaultCommand = ShooterIdleCommand()
     ControlBoard.startShooter
-        .whileActiveOnce(
-            SequentialCommandGroup(
-                SpinUpCommand(),
-                // TODO: Add feed up command here
-                ShootCommand()))
-    //    ControlBoard.idleShooter.whileActiveOnce(IdleCommand())
+      .whileActiveOnce(
+        SpinUpCommand().alongWith(ShootCommand())
+      )
 
     Intake.defaultCommand = IntakeIdleCommand()
-
-    ControlBoard.runIntake.whileActiveContinuous(IntakeBallsCommand())
+    ControlBoard.runIntake.whileActiveContinuous(IntakeBallsCommand().alongWith(FeederSerialize()))
     //        .whenInactive(LiftIntakeCommand())
     ControlBoard.prepareClimb.whileActiveContinuous(PrepareClimbCommand())
+
+    Feeder.defaultCommand = FeederIdleCommand()
+    ControlBoard.runFeederIn.whileActiveOnce(FeederCommand(FeederConstants.FeederState.FORWARD_ALL))
+    ControlBoard.runFeederOut
+      .whileActiveOnce(FeederCommand(FeederConstants.FeederState.BACKWARD_ALL))
+
   }
 
   override fun robotInit() {
--- src/main/kotlin/com/team4099/robot2022/commands/shooter/ShootCommand.kt
+++ src/main/kotlin/com/team4099/robot2022/commands/shooter/ShootCommand.kt
@@ -1,17 +1,2 @@
-package com.team4099.robot2022.commands.shooter
+package com.team4099.robot2022.commands.shooter 
 
-import com.team4099.robot2022.subsystems.Shooter
-import edu.wpi.first.wpilibj2.command.CommandBase
-
-class ShootCommand : CommandBase() {
-  init {
-    addRequirements(Shooter)
-    // addRequirements(Feeder)
-  }
-
-  override fun execute() {}
-
-  override fun isFinished(): Boolean {
-    return false
-  }
-}
