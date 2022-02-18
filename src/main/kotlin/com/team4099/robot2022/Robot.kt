3214eaf78fb77b356bcd94cdfd8982a6294bf6b8
flash76 <30060599+flash76@users.noreply.github.com>
2022-02-18 17:08:35 -0500

    fix idle command

--- src/main/kotlin/com/team4099/robot2022/Robot.kt
+++ src/main/kotlin/com/team4099/robot2022/Robot.kt
@@ -4,11 +4,13 @@ import com.team4099.lib.logging.Logger
 import com.team4099.lib.smoothDeadband
 import com.team4099.robot2022.commands.drivetrain.OpenLoopDriveCommand
 import com.team4099.robot2022.commands.drivetrain.ResetGyroCommand
+import com.team4099.robot2022.commands.shooter.ShooterIdleCommand
 import com.team4099.robot2022.commands.shooter.ShootCommand
 import com.team4099.robot2022.commands.shooter.SpinUpCommand
 import com.team4099.robot2022.config.Constants
 import com.team4099.robot2022.config.ControlBoard
 import com.team4099.robot2022.subsystems.Drivetrain
+import com.team4099.robot2022.subsystems.Shooter
 import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.RobotController
 import edu.wpi.first.wpilibj.TimedRobot
@@ -21,34 +23,36 @@ object Robot : TimedRobot() {
 
   init {
     val robotId =
-        Constants.Tuning
-            .ROBOT_ID_PINS
-            .withIndex()
-            .map { (i, pin) -> if (DigitalInput(pin).get()) 0 else 2.0.pow(i).toInt() }
-            .sum()
+      Constants.Tuning
+        .ROBOT_ID_PINS
+        .withIndex()
+        .map { (i, pin) -> if (DigitalInput(pin).get()) 0 else 2.0.pow(i).toInt() }
+        .sum()
     robotName =
-        Constants.Tuning.ROBOT_ID_MAP.getOrDefault(robotId, Constants.Tuning.RobotName.COMPETITION)
+      Constants.Tuning.ROBOT_ID_MAP.getOrDefault(robotId, Constants.Tuning.RobotName.COMPETITION)
     Logger.addEvent("Robot", "Robot Construction (running on $robotName)")
     Logger.addSource("Robot", "Battery Voltage", RobotController::getBatteryVoltage)
 
     Logger.startLogging()
 
     Drivetrain.defaultCommand =
-        OpenLoopDriveCommand(
-            { ControlBoard.strafe.smoothDeadband(Constants.Joysticks.THROTTLE_DEADBAND) },
-            { ControlBoard.forward.smoothDeadband(Constants.Joysticks.THROTTLE_DEADBAND) },
-            { ControlBoard.turn.smoothDeadband(Constants.Joysticks.TURN_DEADBAND) })
+      OpenLoopDriveCommand(
+        { ControlBoard.strafe.smoothDeadband(Constants.Joysticks.THROTTLE_DEADBAND) },
+        { ControlBoard.forward.smoothDeadband(Constants.Joysticks.THROTTLE_DEADBAND) },
+        { ControlBoard.turn.smoothDeadband(Constants.Joysticks.TURN_DEADBAND) })
 
     ControlBoard.resetGyro.whileActiveOnce(ResetGyroCommand())
 
     // Shooter controls
-    //    Shooter.defaultCommand = IdleCommand()
+    Shooter.defaultCommand = ShooterIdleCommand()
     ControlBoard.startShooter
-        .whileActiveOnce(
-            SequentialCommandGroup(
-                SpinUpCommand(),
-                // TODO: Add feed up command here
-                ShootCommand()))
+      .whileActiveOnce(
+        SequentialCommandGroup(
+          SpinUpCommand(),
+          // TODO: Add feed up command here
+          ShootCommand()
+        )
+      )
     //    ControlBoard.idleShooter.whileActiveOnce(IdleCommand())
   }
 
--- src/main/kotlin/com/team4099/robot2022/commands/shooter/ShooterIdleCommand.kt
+++ src/main/kotlin/com/team4099/robot2022/commands/shooter/ShooterIdleCommand.kt
@@ -4,13 +4,13 @@ import com.team4099.robot2022.config.ShooterConstants
 import com.team4099.robot2022.subsystems.Shooter
 import edu.wpi.first.wpilibj2.command.CommandBase
 
-class IdleCommand : CommandBase() {
+class ShooterIdleCommand : CommandBase() {
   init {
     addRequirements(Shooter)
   }
 
   override fun initialize() {
-    Shooter.shooterState = ShooterConstants.ShooterState.IDLE
+    Shooter.shooterState = ShooterConstants.ShooterState.OFF
   }
 
   override fun isFinished(): Boolean = false
