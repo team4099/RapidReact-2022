d3ba6bb873f338df3de20301130bd1a559d5a445
flash76 <30060599+flash76@users.noreply.github.com>
2022-02-18 17:18:44 -0500

    merge main into shooter heehee

--- src/main/kotlin/com/team4099/robot2022/Robot.kt
+++ src/main/kotlin/com/team4099/robot2022/Robot.kt
@@ -2,15 +2,31 @@ package com.team4099.robot2022
 
 import com.team4099.lib.logging.Logger
 import com.team4099.lib.smoothDeadband
+import com.team4099.robot2021.subsystems.com.team4099.robot2022.subsystems.Intake
+import com.team4099.robot2022.auto.AutonomousSelector
 import com.team4099.robot2022.commands.drivetrain.OpenLoopDriveCommand
 import com.team4099.robot2022.commands.drivetrain.ResetGyroCommand
-import com.team4099.robot2022.config.Constants
+import com.team4099.robot2022.commands.feeder.FeederCommand
+import com.team4099.robot2022.commands.feeder.FeederSerialize
+import com.team4099.robot2022.commands.intake.IntakeBallsCommand
+import com.team4099.robot2022.commands.intake.IntakeIdleCommand
+import com.team4099.robot2022.commands.intake.PrepareClimbCommand
+import com.team4099.robot2022.commands.shooter.ShootCommand
+import com.team4099.robot2022.commands.shooter.ShooterIdleCommand
+import com.team4099.robot2022.commands.shooter.SpinUpCommand
 import com.team4099.robot2022.config.ControlBoard
+import com.team4099.robot2022.config.constants.Constants
+import com.team4099.robot2022.config.constants.FeederConstants
 import com.team4099.robot2022.subsystems.Drivetrain
+import com.team4099.robot2022.subsystems.Feeder
+import com.team4099.robot2022.subsystems.Shooter
+import edu.wpi.first.wpilibj.Compressor
 import edu.wpi.first.wpilibj.DigitalInput
+import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.RobotController
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj2.command.CommandScheduler
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import kotlin.math.pow
 
 object Robot : TimedRobot() {
@@ -19,13 +35,13 @@ object Robot : TimedRobot() {
 
   init {
     val robotId =
-      Constants.Tuning
-        .ROBOT_ID_PINS
-        .withIndex()
-        .map { (i, pin) -> if (DigitalInput(pin).get()) 0 else 2.0.pow(i).toInt() }
-        .sum()
+        Constants.Tuning
+            .ROBOT_ID_PINS
+            .withIndex()
+            .map { (i, pin) -> if (DigitalInput(pin).get()) 0 else 2.0.pow(i).toInt() }
+            .sum()
     robotName =
-      Constants.Tuning.ROBOT_ID_MAP.getOrDefault(robotId, Constants.Tuning.RobotName.COMPETITION)
+        Constants.Tuning.ROBOT_ID_MAP.getOrDefault(robotId, Constants.Tuning.RobotName.COMPETITION)
     Logger.addEvent("Robot", "Robot Construction (running on $robotName)")
     Logger.addSource("Robot", "Battery Voltage", RobotController::getBatteryVoltage)
 
@@ -48,13 +64,11 @@ object Robot : TimedRobot() {
     // Shooter controls
     Shooter.defaultCommand = ShooterIdleCommand()
     ControlBoard.startShooter
-      .whileActiveOnce(
-        SequentialCommandGroup(
-          SpinUpCommand(),
-          // TODO: Add feed up command here
-          ShootCommand()
-        )
-      )
+        .whileActiveOnce(
+            SequentialCommandGroup(
+                SpinUpCommand(),
+                // TODO: Add feed up command here
+                ShootCommand()))
     //    ControlBoard.idleShooter.whileActiveOnce(IdleCommand())
 
     Intake.defaultCommand = IntakeIdleCommand()
