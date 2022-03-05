e63f633f7cab9cabdec34aa9aa16773e46f8e870
Parth Oza <plusparth@gmail.com>
2022-03-04 23:37:34 -0500

    Move soft limits to subsystem from command

---
+++ src/main/kotlin/com/team4099/robot2022/commands/climber/OpenLoopClimbCommand.kt
@@ -0,0 +1,22 @@
+package com.team4099.robot2022.commands.climber
+
+import com.team4099.lib.logging.Logger
+import com.team4099.robot2022.config.constants.TelescopingClimberConstants
+import com.team4099.robot2022.subsystems.TelescopingClimber
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class SpoolBothDownCommand : CommandBase() {
+  init {
+    addRequirements(TelescopingClimber)
+  }
+
+  override fun execute() {
+    TelescopingClimber.setOpenLoop(-1.0, -1.0)
+
+    Logger.addEvent("Climber", "Telescoping climber spooling down")
+  }
+
+  override fun end(interrupted: Boolean) {
+    TelescopingClimber.setOpenLoop(0.0, 0.0)
+  }
+}
