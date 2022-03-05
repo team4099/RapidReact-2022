359ab7574a80cb798224e3f16d1f132195486ee1
Eckart Schneider <eckshn@gmail.com>
2022-03-04 22:57:34 -0500

    Manually code soft limit

--- src/main/kotlin/com/team4099/robot2022/commands/climber/SpoolBothDownCommand.kt
+++ src/main/kotlin/com/team4099/robot2022/commands/climber/SpoolBothDownCommand.kt
@@ -1,6 +1,7 @@
 package com.team4099.robot2022.commands.climber
 
 import com.team4099.lib.logging.Logger
+import com.team4099.robot2022.config.constants.TelescopingClimberConstants
 import com.team4099.robot2022.subsystems.TelescopingClimber
 import edu.wpi.first.wpilibj2.command.CommandBase
 
@@ -10,7 +11,22 @@ class SpoolBothDownCommand : CommandBase() {
   }
 
   override fun execute() {
-    TelescopingClimber.setOpenLoop(-0.5, -0.5)
+    if (TelescopingClimber.telescopingLeftArmSensor.position <
+        TelescopingClimberConstants.REVERSE_SOFT_LIMIT) {
+      if (TelescopingClimber.telescopingRightArmSensor.position <
+          TelescopingClimberConstants.REVERSE_SOFT_LIMIT) {
+        TelescopingClimber.setOpenLoop(-0.5, -0.5)
+      } else {
+        TelescopingClimber.setOpenLoop(-0.5, 0.0)
+      }
+    } else {
+      if (TelescopingClimber.telescopingRightArmSensor.position <
+          TelescopingClimberConstants.REVERSE_SOFT_LIMIT) {
+        TelescopingClimber.setOpenLoop(0.0, -0.5)
+      } else {
+        TelescopingClimber.setOpenLoop(0.0, 0.0)
+      }
+    }
 
     Logger.addEvent("Climber", "Telescoping climber spooling down")
   }
