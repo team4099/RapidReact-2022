9a46290b619ac72d68fa1ead8430ed5cbdafa6f6
Eckart Schneider <eckshn@gmail.com>
2022-03-08 14:48:40 -0500

    Run spotless apply

--- src/main/kotlin/com/team4099/robot2022/auto/mode/OneBallFenderShotThenTaxi.kt
+++ src/main/kotlin/com/team4099/robot2022/auto/mode/OneBallFenderShotThenTaxi.kt
@@ -4,7 +4,6 @@ import com.team4099.lib.units.derived.Angle
 import com.team4099.robot2022.commands.drivetrain.OpenLoopReverseCommand
 import com.team4099.robot2022.commands.drivetrain.ResetGyroCommand
 import com.team4099.robot2022.commands.shooter.ShootCommand
-import com.team4099.robot2022.commands.shooter.ShooterIdleCommand
 import com.team4099.robot2022.commands.shooter.SpinUpNearCommand
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
