621e6ec26cdc6e610dc01f84741984ee4fec1cad
sswadkar <saraanshwadkar@gmail.com>
2022-03-23 16:25:36 -0400

    Add timer threshold for intake LED

--- src/main/kotlin/com/team4099/robot2022/subsystems/intake/Intake.kt
+++ src/main/kotlin/com/team4099/robot2022/subsystems/intake/Intake.kt
@@ -1,5 +1,7 @@
 package com.team4099.robot2022.subsystems.intake
 
+import com.team4099.lib.hal.Clock
+import com.team4099.lib.units.base.inSeconds
 import com.team4099.robot2022.config.constants.IntakeConstants
 import com.team4099.robot2022.config.constants.LEDConstants
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -24,8 +26,11 @@ class Intake(val io: IntakeIO) : SubsystemBase() {
   val hasBall: Boolean
     get() {
       return inputs.rollerStatorCurrent >= LEDConstants.INTAKE_CURRENT_THRESHOLD &&
-          rollerState == IntakeConstants.RollerState.IN
+          rollerState == IntakeConstants.RollerState.IN &&
+          ((Clock.fpgaTime - extendTime).inSeconds >= IntakeConstants.RAMP_TIME)
     }
+
+  var extendTime = Clock.fpgaTime
 
   init {
     // necessary because the setter is not called on initialization
