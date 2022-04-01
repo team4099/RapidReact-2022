8ecd2d7dcf8e35e56f274a0521d4a6279e2ba95e
sswadkar <saraanshwadkar@gmail.com>
2022-04-01 17:08:06 -0400

    Set up new candle branch

--- src/main/kotlin/com/team4099/robot2022/config/constants/LEDConstants.kt
+++ src/main/kotlin/com/team4099/robot2022/config/constants/LEDConstants.kt
@@ -7,30 +7,30 @@ object LEDConstants {
   val INTAKE_CURRENT_THRESHOLD = 15.amps
   val OUTAKE_CURRENT_THRESHOLD = 20.amps
 
-  enum class LEDState(val blinkinMode: BlinkinLedDriver.BlinkinLedMode) {
+  enum class LEDState(val red: Int, val green: Int, val blue: Int) {
     // Solid Orange (never used)
-    IDLE(BlinkinLedDriver.BlinkinLedMode.SOLID_ORANGE),
+    IDLE(0, 0, 0),
 
     // Blinking Gold
-    AUTO(BlinkinLedDriver.BlinkinLedMode.FIXED_STROBE_GOLD),
+    AUTO(0, 0, 0),
 
     // Solid Gold
-    STANDING_ZERO(BlinkinLedDriver.BlinkinLedMode.SOLID_GOLD),
+    STANDING_ZERO(0, 0, 0),
 
     // Solid Purple
-    STANDING_ONE(BlinkinLedDriver.BlinkinLedMode.SOLID_VIOLET),
+    STANDING_ONE(0, 0, 0),
 
     // Solid Green
-    STANDING_TWO(BlinkinLedDriver.BlinkinLedMode.SOLID_GREEN),
+    STANDING_TWO(0, 0, 0),
 
     // Solid Blue
-    INTAKING(BlinkinLedDriver.BlinkinLedMode.SOLID_BLUE),
+    INTAKING(0, 0, 0),
 
     // Solid Red
-    OUTTAKING(BlinkinLedDriver.BlinkinLedMode.SOLID_DARK_RED),
+    OUTTAKING(0, 0, 0),
 
     // Changing Green
-    CLIMBER_READY(BlinkinLedDriver.BlinkinLedMode.FIXED_BEATS_FOREST),
+    CLIMBER_READY(0, 0, 0),
 
     // Changing Mix
     CLIMBING(BlinkinLedDriver.BlinkinLedMode.FIXED_BEATS_PARTY),
