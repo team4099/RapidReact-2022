781611a68b3788be2d4f22c221cd152c096ee28d
Eckart Schneider <eckshn@gmail.com>
2022-04-07 18:09:03 -0400

    Tune timing for auto climb for actual competition bars

--- src/main/kotlin/com/team4099/robot2022/commands/climber/ClimbSequenceCommand.kt
+++ src/main/kotlin/com/team4099/robot2022/commands/climber/ClimbSequenceCommand.kt
@@ -15,6 +15,8 @@ class ClimbSequenceCommand(
       RetractTelescopingArmCommand(telescopingClimber),
       ExtendPivotArmCommand(pivotClimber),
       WaitCommand(0.5),
+      ExtendTelescopingArmCommand(telescopingClimber).withTimeout(0.25),
+      WaitCommand(0.5),
       ExtendTelescopingArmCommand(telescopingClimber),
       RetractPivotArmCommand(pivotClimber),
       WaitCommand(1.0)
