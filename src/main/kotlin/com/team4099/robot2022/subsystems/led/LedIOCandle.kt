f31939e18f3656d65f853853eb2547a35593cd75
sswadkar <saraanshwadkar@gmail.com>
2022-04-20 23:29:26 -0400

    Spotless apply

--- src/main/kotlin/com/team4099/robot2022/subsystems/led/LedIOCandle.kt
+++ src/main/kotlin/com/team4099/robot2022/subsystems/led/LedIOCandle.kt
@@ -9,8 +9,7 @@ import com.team4099.robot2022.config.constants.LEDConstants.CandleState
 object LedIOCandle : LedIO {
   private val ledController = CANdle(Constants.LED.LED_CONTROLLER_ID)
 
-  private var lastLedState: BlinkinLedDriver.BlinkinLedMode =
-    BlinkinLEDState.IDLE.blinkinMode
+  private var lastLedState: BlinkinLedDriver.BlinkinLedMode = BlinkinLEDState.IDLE.blinkinMode
 
   override fun updateInputs(inputs: LedIO.LedIOInputs) {
     inputs.ledState = lastLedState.name
@@ -19,26 +18,16 @@ object LedIOCandle : LedIO {
   override fun setMode(mode: BlinkinLedDriver.BlinkinLedMode) {
     lastLedState = mode
     when (mode) {
-      BlinkinLEDState.IDLE.blinkinMode ->
-        setCandleState(CandleState.IDLE)
-      BlinkinLEDState.AUTO.blinkinMode ->
-        setCandleState(CandleState.AUTO)
-      BlinkinLEDState.STANDING_ZERO.blinkinMode ->
-        setCandleState(CandleState.STANDING_ZERO)
-      BlinkinLEDState.STANDING_ONE.blinkinMode ->
-        setCandleState(CandleState.STANDING_ONE)
-      BlinkinLEDState.STANDING_TWO.blinkinMode ->
-        setCandleState(CandleState.STANDING_TWO)
-      BlinkinLEDState.INTAKING.blinkinMode ->
-        setCandleState(CandleState.INTAKING)
-      BlinkinLEDState.OUTTAKING.blinkinMode ->
-        setCandleState(CandleState.OUTTAKING)
-      BlinkinLEDState.CLIMBER_READY.blinkinMode ->
-        setCandleState(CandleState.CLIMBER_READY)
-      BlinkinLEDState.CLIMBING.blinkinMode ->
-        setCandleState(CandleState.CLIMBING)
-      BlinkinLEDState.DISALLOW_CLIMB.blinkinMode ->
-        setCandleState(CandleState.DISALLOW_CLIMB)
+      BlinkinLEDState.IDLE.blinkinMode -> setCandleState(CandleState.IDLE)
+      BlinkinLEDState.AUTO.blinkinMode -> setCandleState(CandleState.AUTO)
+      BlinkinLEDState.STANDING_ZERO.blinkinMode -> setCandleState(CandleState.STANDING_ZERO)
+      BlinkinLEDState.STANDING_ONE.blinkinMode -> setCandleState(CandleState.STANDING_ONE)
+      BlinkinLEDState.STANDING_TWO.blinkinMode -> setCandleState(CandleState.STANDING_TWO)
+      BlinkinLEDState.INTAKING.blinkinMode -> setCandleState(CandleState.INTAKING)
+      BlinkinLEDState.OUTTAKING.blinkinMode -> setCandleState(CandleState.OUTTAKING)
+      BlinkinLEDState.CLIMBER_READY.blinkinMode -> setCandleState(CandleState.CLIMBER_READY)
+      BlinkinLEDState.CLIMBING.blinkinMode -> setCandleState(CandleState.CLIMBING)
+      BlinkinLEDState.DISALLOW_CLIMB.blinkinMode -> setCandleState(CandleState.DISALLOW_CLIMB)
       else -> setCandleState(CandleState.IDLE)
     }
   }
