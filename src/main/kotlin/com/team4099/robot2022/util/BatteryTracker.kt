39ba97b335757ec6b0c69a34d608fcb082e88241
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-09-29 13:37:22 -0400

    revert back pls work

--- src/main/kotlin/com/team4099/robot2022/util/BatteryTracker.kt
+++ src/main/kotlin/com/team4099/robot2022/util/BatteryTracker.kt
@@ -7,12 +7,13 @@ object BatteryTracker {
   private var name: String = "bit.ly/BAT-00000000"
   private val scanCommand =
     byteArrayOf(0x7e, 0x00, 0x08, 0x01, 0x00, 0x02, 0x01, 0xab.toByte(), 0xcd.toByte())
+
   private val responsePrefix = byteArrayOf(0x02, 0x00, 0x00, 0x01, 0x00, 0x33, 0x31)
   private val fullResponseLength = responsePrefix.size + nameLength + 1
 
   fun scanBattery(timeout: Double): String {
     try {
-      val port: SerialPort = SerialPort(9600, SerialPort.Port.kUSB)
+      val port: SerialPort = SerialPort(9600, SerialPort.Port.kUSB2)
 
       port.setTimeout(timeout)
       port.setWriteBufferSize(scanCommand.size)
@@ -42,7 +43,7 @@ object BatteryTracker {
       // Read name from data
       val batteryNameBytes: ByteArray = ByteArray(nameLength)
       System.arraycopy(response, responsePrefix.size, batteryNameBytes, 0, nameLength)
-      name = String(batteryNameBytes).substring(7, name.length)
+      //      name = String(batteryNameBytes).substring(7, name.length)
       println("[BatteryTracker] Scanned battery " + name)
     } catch (e: Exception) {
       println("[BatteryTracker] Exception while trying to scan battery")
