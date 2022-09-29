790b20c946eae75c53248035da7ad0d508a9ba10
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-09-29 14:10:35 -0400

    im switching to google drive again

--- src/main/kotlin/com/team4099/robot2022/RobotContainer.kt
+++ src/main/kotlin/com/team4099/robot2022/RobotContainer.kt
@@ -158,13 +158,7 @@ object RobotContainer {
     ControlBoard.retractTelescoping.whileActiveOnce(OpenLoopClimbCommand(telescopingClimber))
     ControlBoard.extendPivot.whileActiveOnce(ExtendPivotArmCommand(pivotClimber))
     ControlBoard.retractPivot.whileActiveOnce(RetractPivotArmCommand(pivotClimber))
-    ControlBoard.startClimbSequence.whileActiveOnce(
-      UseLowThresholdClimbCommand(pneumatic)
-        .alongWith(
-          ResetGyroPitchCommand(drivetrain)
-            .andThen(ClimbSequenceCommand(telescopingClimber, pivotClimber))
-        )
-    )
+    ControlBoard.startClimbSequence.whileActiveOnce(UseLowThresholdClimbCommand(pneumatic).alongWith(ResetGyroPitchCommand(drivetrain).andThen(ClimbSequenceCommand(telescopingClimber, pivotClimber))))
     ControlBoard.fixTelescopingRung.whileActiveOnce(
       FixTelescopingInFrontOfRungCommand(telescopingClimber)
     )
