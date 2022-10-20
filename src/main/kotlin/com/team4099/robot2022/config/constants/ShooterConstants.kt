15afe91d48aeb557e64e2cbf764973844b4975ff
Matthew Choulas <MatthewChoulas@gmail.com>
2022-10-19 22:36:50 -0400

    shooter pid tuning changes

--- src/main/kotlin/com/team4099/robot2022/config/constants/ShooterConstants.kt
+++ src/main/kotlin/com/team4099/robot2022/config/constants/ShooterConstants.kt
@@ -8,7 +8,7 @@ import com.team4099.lib.units.perMinute
 
 object ShooterConstants {
   // TODO change PID, ks, and kv values
-  const val SHOOTER_FLYWHEEL_KP = 0.075
+  const val SHOOTER_FLYWHEEL_KP = 0.15
   const val SHOOTER_FLYWHEEL_KI = 0.0
   const val SHOOTER_FLYWHEEL_KD = 7.0
 
@@ -49,7 +49,7 @@ object ShooterConstants {
       )
     ), // TODO: Fix with a better idle value
     // 1200,-3600 gave a lot more arc
-    SPIN_UP_UPPER(Pair(1950.0.rotations.perMinute, -2100.0.rotations.perMinute)),
+    SPIN_UP_UPPER(Pair(1650.0.rotations.perMinute, -2100.0.rotations.perMinute)),
     SPIN_UP_LOWER(Pair(1100.0.rotations.perMinute, -1100.0.rotations.perMinute)),
     SHOOTER_UNJAM(Pair(-2000.0.rotations.perMinute, -2000.0.rotations.perMinute))
   }
